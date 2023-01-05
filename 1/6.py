dde27cc795b4cc4a014a11b9d5e7bd43b7a1f0c4
Alchan-Hong <122035213+Alchan-Hong@users.noreply.github.com>
2023-01-06 04:05:22 +0900

    Add files via upload

---
+++ 1/6.py
@@ -0,0 +1,15 @@
+import turtle
+t = turtle.Turtle()
+
+t.shape("turtle")
+t.left(90)
+t.forward(200)
+t.right(90)
+t.forward(300)
+t.right(90)
+t.forward(200)
+t.right(90)
+t.forward(300)
+
+turtle.mainloop()
+turtle.bye()
